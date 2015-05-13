35b0dca60671deb56daa0d6e5969e14d1f12ad36
Joe Littlejohn <joelittlejohn@gmail.com>
2015-05-13 10:39:45 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-http-hystrix "0.1.0-SNAPSHOT"
+(defproject clj-http-hystrix "0.1.0"
 
   :description "A Clojure library to wrap clj-http requests as hystrix commands"
   :url "https://github.com/joelittlejohn/clj-http-hystrix"
