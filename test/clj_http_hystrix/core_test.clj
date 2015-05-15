5aac24efc4a20830d5296a7b4265b9feb2906e16
mtinsley <mark.tinsley@nokia.com>
2015-05-15 20:46:35 +0100

    bug fix: corrected handling of ExceptionInfo

--- test/clj_http_hystrix/core_test.clj
+++ test/clj_http_hystrix/core_test.clj
@@ -10,13 +10,24 @@
                                           :hystrix/fallback-fn (constantly "foo")})
       => "foo")
 
+(fact "hystrix wrapping return successful call"
+      (-> (http/get "http://httpstat.us/200" {:throw-exceptions false
+                                              :hystrix/fallback-fn (constantly "foo")})
+          :status)
+      => 200)
+
 (fact "hystrix wrapping with exceptions off"
       (-> (http/get "http://httpstat.us/400" {:throw-exceptions false
                                               :hystrix/command-key :foo}) :status)
       => 400)
 
-(fact "hystrix wrapping with exceptions on"
+(fact "hystrix wrapping with exceptions implicitly on"
       (http/get "http://httpstat.us/400" {:hystrix/command-key :foo})
+      => (throws clojure.lang.ExceptionInfo "clj-http: status 400"))
+
+(fact "hystrix wrapping with exceptions explicitly on"
+      (http/get "http://httpstat.us/400" {:throw-exceptions true
+                                          :hystrix/command-key :foo})
       => (throws clojure.lang.ExceptionInfo "clj-http: status 400"))
 
 (fact "request with no hystrix key present"
