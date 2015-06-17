8a1717f47ff3b74e12616cec15f4d307ee5fa977
Joe Littlejohn <joelittlejohn@gmail.com>
2015-06-17 23:55:11 +0100

    Replace stale deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[clj-http "1.0.0"]
-                 [com.netflix.hystrix/hystrix-clj "1.4.7"]
+  :dependencies [[clj-http "1.1.2"]
+                 [com.netflix.hystrix/hystrix-clj "1.4.11"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [robert/hooke "1.3.0"]
