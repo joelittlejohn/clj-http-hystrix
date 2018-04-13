f1cb813bbfe4175b5337a1e99438e8e3cbeb4c19
Ulf Gitschthaler <ulf.gitschthaler@exaring.de>
2018-04-13 20:18:30 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[clj-http "2.3.0"]
-                 [com.netflix.hystrix/hystrix-clj "1.4.23"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[clj-http "3.8.0"]
+                 [com.netflix.hystrix/hystrix-clj "1.5.12"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [robert/hooke "1.3.0"]
                  [slingshot "0.12.2"]]
 
@@ -17,8 +17,8 @@
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]]
 
-  :profiles {:dev {:dependencies [[midje "1.8.3"]
-                                  [rest-cljer "0.1.22" :exclusions [clj-http]]]
-                   :plugins [[lein-cloverage "1.0.2"]
-                             [lein-environ "1.0.0"]
-                             [lein-midje "3.2"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.1"]
+                                  [rest-cljer "0.2.2" :exclusions [clj-http]]]
+                   :plugins [[lein-cloverage "1.0.10"]
+                             [lein-environ "1.1.0"]
+                             [lein-midje "3.2.1"]]}})
