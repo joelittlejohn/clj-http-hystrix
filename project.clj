d92d5d38e8aaf0b5916078f07adb88189a31cc39
Joe Littlejohn <joelittlejohn@gmail.com>
2019-10-27 21:52:31 +0000

    Dependency upgrade

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[clj-http "3.8.0"]
-                 [com.netflix.hystrix/hystrix-clj "1.5.12"]
-                 [org.clojure/tools.logging "0.4.0"]
+  :dependencies [[clj-http "3.10.0"]
+                 [com.netflix.hystrix/hystrix-clj "1.5.18"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [robert/hooke "1.3.0"]
                  [slingshot "0.12.2"]]
 
@@ -16,8 +16,8 @@
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [midje "1.9.1"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [midje "1.9.9"]
                                   [rest-cljer "0.2.2" :exclusions [clj-http]]]
                    :plugins [[lein-cloverage "1.0.10"]
                              [lein-environ "1.1.0"]
